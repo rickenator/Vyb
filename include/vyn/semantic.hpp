1420649e1183ae4e6230e2a48d8fb9a3fc68d18f
Rick Goldberg <rick@aniviza.com>
2025-10-19 13:22:38 -0700

    refactor: Update semantic.hpp comment to use aspect/bind terminology
    Changed 'trait implementations' to 'aspect implementations' in comment
    Updated example from 'impl for Box<T>' to 'bind Display -> Box<T>'
    
    Completes aspect/bind terminology consistency in all header files.

--- include/vyn/semantic.hpp
+++ include/vyn/semantic.hpp
@@ -389,8 +389,8 @@ private:
     // Struct field type storage for member access resolution
     std::unordered_map<std::string, std::map<std::string, ast::TypeNode*>> structFieldTypes;
     
-    // Context for resolving 'Self' type in trait implementations
-    ast::TypeNode* currentImplType = nullptr;  // Set to Box<T> when processing impl for Box<T>
+    // Context for resolving 'Self' type in aspect implementations
+    ast::TypeNode* currentImplType = nullptr;  // Set to Box<T> when processing bind Display -> Box<T>
 
     void enterScope();
     void exitScope();
